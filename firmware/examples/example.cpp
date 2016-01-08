7a7c877ae810899c9f8c3301fcf9c8736b9bd4a7
ceajog <gustavo.gonnet@ericsson.com>
2016-01-07 21:32:47 -0500

    adding example file

---
+++ firmware/examples/example.cpp
@@ -0,0 +1,13 @@
+#include "finite-state-machine/finite-state-machine.h"
+
+// TODO
+// in the meantime, check the example here: http://playground.arduino.cc/Code/FiniteStateMachine#Example
+
+void setup {
+
+}
+
+
+void loop {
+
+}
